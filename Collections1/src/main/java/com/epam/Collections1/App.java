ea90ac8a1b7a649e874e3ff2107d4edb4056ac5c
koranavyasri8966436 <39006082+koranavyasri8966436@users.noreply.github.com>
2020-03-10 19:23:41 +0530

    Collections

---
+++ Collections1/src/main/java/com/epam/Collections1/App.java
@@ -0,0 +1,13 @@
+package com.epam.Collections1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
